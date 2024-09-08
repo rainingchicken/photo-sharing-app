d4b9c03e9e93d1f173f8f6b1840000f81367b553
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-09-08 12:58:45 -0700

    frontend vite react, tailwind, and flowbite react

---
+++ frontend-photo/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "flowbite-react";
+
+const App = () => {
+  return (
+    <div>
+      <Button outline gradientDuoTone="purpleToBlue">
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default App;
