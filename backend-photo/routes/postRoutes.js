d3593c85117c405df04528e3611d1eccee692db5
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-09-10 15:53:31 -0700

    assign user to posts and posts error handling

--- backend-photo/routes/postRoutes.js
+++ backend-photo/routes/postRoutes.js
@@ -2,49 +2,82 @@ import express from "express";
 const router = express.Router();
 import asyncHandler from "express-async-handler";
 import Post from "../models/postModel.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 router.get(
   "/",
+  protect,
   asyncHandler(async (req, res) => {
-    const allPosts = await Post.find({});
-    res.status(200).json(allPosts);
+    const user = req.user.id;
+    const allPosts = await Post.find({ user }).sort({ createdAt: -1 });
+    if (allPosts) {
+      res.status(200).json(allPosts);
+    } else {
+      res.status(404);
+      throw new Error("Cannot find posts");
+    }
   })
 );
 
 router.get(
   "/:_id",
+  protect,
   asyncHandler(async (req, res) => {
     const _id = req.params;
     const aPost = await Post.findById(_id);
-    res.status(200).json(aPost);
+    if (aPost) {
+      res.status(200).json(aPost);
+    } else {
+      res.status(404);
+      throw new Error("Cannot find the post");
+    }
   })
 );
 
 router.post(
   "/",
+  protect,
   asyncHandler(async (req, res) => {
+    const user = req.user.id;
     const { title, image } = req.body;
-    const createdPost = await Post.create({ title, image });
-    res.status(200).json(createdPost);
+    const createdPost = await Post.create({ title, image, user });
+    if (createdPost) {
+      res.status(200).json(createdPost);
+    } else {
+      res.status(404);
+      throw new Error("Cannot create the post");
+    }
   })
 );
 
 router.patch(
   "/:_id",
+  protect,
   asyncHandler(async (req, res) => {
     const _id = req.params;
     await Post.findByIdAndUpdate(_id, req.body);
     const editedPost = await Post.findById(_id);
-    res.status(200).json(editedPost);
+    if (editedPost) {
+      res.status(200).json(editedPost);
+    } else {
+      res.status(404);
+      throw new Error("Cannot update the post");
+    }
   })
 );
 
 router.delete(
   "/:_id",
+  protect,
   asyncHandler(async (req, res) => {
     const _id = req.params;
     const deletedPost = await Post.findByIdAndDelete(_id);
-    res.status(200).json(deletedPost);
+    if (deletedPost) {
+      res.status(200).json(deletedPost);
+    } else {
+      res.status(404);
+      throw new Error("Cannot delete the post");
+    }
   })
 );
 
