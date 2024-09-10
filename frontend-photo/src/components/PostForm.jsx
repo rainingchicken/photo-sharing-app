e0e57a99070ba257f45f7bd3c05220cf39364853
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-09-09 21:23:42 -0700

    if user changes route while form is not empty, prompt if want to leave and delete image from firebase if leaving

--- frontend-photo/src/components/PostForm.jsx
+++ frontend-photo/src/components/PostForm.jsx
@@ -1,4 +1,4 @@
-import { Button, FileInput, Label } from "flowbite-react";
+import { Button, FileInput, Spinner } from "flowbite-react";
 import { useState } from "react";
 import { useCreateAPostMutation } from "../slices/postApiSlice";
 import { toast } from "react-toastify";
@@ -8,14 +8,18 @@ import {
   ref,
   uploadBytesResumable,
   getDownloadURL,
+  deleteObject,
 } from "firebase/storage";
+import { useBeforeUnload, useBlocker } from "react-router-dom";
+import { Alert } from "flowbite-react";
 
 const PostForm = () => {
   const [title, setTitle] = useState("");
   const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState(null);
   const [image, setImage] = useState(null);
 
-  const [createPostAPICall] = useCreateAPostMutation();
+  const [createPostAPICall, { isLoading }] = useCreateAPostMutation();
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
@@ -32,6 +36,7 @@ const PostForm = () => {
       }
       const storage = getStorage(app);
       const fileName = `${Date.now()}-${file.name}`;
+      setFileName(fileName);
       const storageRef = ref(storage, fileName);
       const uploadTask = uploadBytesResumable(storageRef, file);
       uploadTask.on(
@@ -43,6 +48,7 @@ const PostForm = () => {
           switch (snapshot.state) {
             case "paused":
               console.log("Upload is paused");
+              toast.info("Upload is paused");
               break;
             case "running":
               console.log("Upload is running");
@@ -51,7 +57,9 @@ const PostForm = () => {
         },
         (error) => {
           // Handle unsuccessful uploads
-          toast.dark("something went wrong cannot upload image");
+          toast.error(
+            "Cannot upload image. Make sure the image file size is less than 2MB"
+          );
           console.log(error);
         },
         () => {
@@ -62,7 +70,7 @@ const PostForm = () => {
         }
       );
     } catch (error) {
-      toast.warning("Something wento wrong cannot upload image");
+      toast.error("Something wento wrong cannot upload image");
       console.log(error);
     }
   };
@@ -76,21 +84,74 @@ const PostForm = () => {
     try {
       const res = await createPostAPICall(newPost).unwrap();
       console.log(res);
-      toast.dark("success post created");
+      toast.success("success post created");
     } catch (err) {
-      toast.warning("Something went wrong. Cannot create post");
+      toast.error("Cannot create the post. Make sure to fill in all fields");
       console.log(err);
     }
   };
 
+  //if user tries to leave route
+  let blocker = useBlocker(
+    ({ currentLocation, nextLocation }) =>
+      image !== null && currentLocation.pathname !== nextLocation.pathname
+  );
+
+  const handleLeavePage = () => {
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName);
+    deleteObject(storageRef)
+      .then(() => {
+        toast.info("Form discarded");
+      })
+      .catch((error) => {
+        toast.error("Something went wrong. Cannot cancel form");
+        console.log(error);
+        blocker.reset();
+      });
+    setImage(null);
+    blocker.proceed();
+    return blocker.state === null;
+  };
+
+  const handleCancel = () => {
+    blocker.reset();
+  };
+
+  //if user refresh or use back button
+  useBeforeUnload(() => handleLeavePage());
+
   return (
     <form onSubmit={handlePostFormSubmit}>
+      {blocker.state === "blocked" ? (
+        <>
+          <Alert color="warning">
+            <span className="font-medium">Alert!</span> You have unsaved
+            changes. Are you sure you want leave this page?
+            <div>
+              <button
+                onClick={handleLeavePage}
+                type="button"
+                className="mr-2 inline-flex items-center rounded-lg bg-cyan-700 px-3 py-1.5 text-center text-xs font-medium text-white hover:bg-cyan-800 focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-800 dark:hover:bg-cyan-900"
+              >
+                Proceed
+              </button>
+              <button
+                type="button"
+                className="rounded-lg border border-cyan-700 bg-transparent px-3 py-1.5 text-center text-xs font-medium text-cyan-700 hover:bg-cyan-800 hover:text-white focus:ring-4 focus:ring-cyan-300 dark:border-cyan-800 dark:text-cyan-800 dark:hover:text-white"
+                onClick={handleCancel}
+              >
+                Cancel
+              </button>
+            </div>
+          </Alert>
+        </>
+      ) : null}
       <input
         type="text"
         placeholder="Sunset at the beach..."
         onChange={handleTitleChange}
       />
-
       <FileInput type="file" accept="image/*" onChange={handleImageChange} />
       <Button onClick={handleUploadImage} gradientDuoTone="purpleToBlue">
         Upload image
@@ -102,9 +163,10 @@ const PostForm = () => {
           className="w-full h-90 object-cover"
         />
       )}
-      <Button type="submit" gradientDuoTone="purpleToBlue">
+      <Button disabled={isLoading} type="submit" gradientDuoTone="purpleToBlue">
         Share!
       </Button>
+      {isLoading ? <Spinner aria-label="Sharing new post" size="xl" /> : <></>}
     </form>
   );
 };
